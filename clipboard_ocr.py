b3df475777c5f51c2b167f6f31b970a880378c98
jarrod <ghbxl@sina.com>
2018-12-02 00:32:16 +0800

    first release

---
+++ clipboard_ocr.py
@@ -0,0 +1,89 @@
+from PIL import Image, ImageGrab
+from aip import *
+import os, json
+import win32clipboard as wc
+import win32con
+
+def get_clipboard_image():
+    file_path = os.path.join(os.path.abspath('.'), 'grabed_img.png')
+    img = ImageGrab.grabclipboard()
+    if isinstance(img, Image.Image):
+        img.save(file_path)
+        return file_path
+    else:
+        return ''
+
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+def baidu_client_create():
+    user_path = os.path.expanduser("~")
+    api_key_file = os.path.join(user_path, 'api_key.json')
+    try:
+        with open(api_key_file, encoding='utf-8') as f:
+            api_key = json.load(f)
+            APP_ID = api_key['BAIDU_APP_ID']
+            API_KEY = api_key['BAIDU_API_KEY']
+            SECRET_KEY = api_key['BAIDU_SECRET_KEY']
+            ocr_client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+            # sr_client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+            return ocr_client
+    except FileNotFoundError:
+        print('api_key_file not found!')
+        return None
+
+def process_result(result):
+    if result:
+        if 'error_code' in result.keys():
+            print('baidu_ocr failed, reason:')
+            print(result['error_msg'])
+            return {'success': False, 'error_code': int(result['error_code']), 'words': ''}  #返回错误码
+        else:
+            words_result = result['words_result']
+            words = ''
+            for wr in words_result:
+                words += wr['words']
+                words += '\n'
+            return {'success': True, 'error_code': -1, 'words': words}
+
+def get_ocr_result(img):
+    client = baidu_client_create()
+    if not client:
+        print('create client failed!')
+        return ""
+    result = process_result(client.basicAccurate(img))
+    # 如果高精度OCR接口日调用次数500次用完了，换普通接口
+    if not result['success']:
+        if result['error_code'] == 17:
+            result = process_result(client.basicGeneral(img))
+            #如果再不行，放弃
+            if not result['success']:
+                return ''
+        else:  #其他错误先不管了
+            return ''
+    else:
+        return result['words']
+
+def set_clip_board(text):
+    wc.OpenClipboard()
+    wc.EmptyClipboard()
+    wc.SetClipboardText(text)
+    wc.CloseClipboard()
+    return text
+
+def do_ocr(img_path):
+    if not img_path:
+        print('get clipboard image failed!')
+        return ''
+    else:
+        img = get_file_content(img_path)
+        result = get_ocr_result(img)
+        print(result)
+        set_clip_board(result)
+        print('已复制到剪切板！')
+        return result
+
+if __name__ == '__main__':
+    img_path = get_clipboard_image()
+    do_ocr(img_path)
---
+++ ocr_gui.py
@@ -0,0 +1,101 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import sys
+import clipboard_ocr as ocr
+
+class OcrGui(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.statusBar()
+
+        exitAction = QAction('退出', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('退出程序')
+        exitAction.triggered.connect(self.close)
+
+        openAction = QAction('打开图片', self)
+        openAction.setShortcut('Ctrl+O')
+        openAction.setStatusTip('打开本地图片进行OCR识别')
+        openAction.triggered.connect(self.native_pic_ocr)
+
+        clipboardAction = QAction('识别剪切板图片', self)
+        clipboardAction.setShortcut('Ctrl+S')
+        clipboardAction.setStatusTip('对剪切板的图片进行OCR识别')
+        clipboardAction.triggered.connect(self.clipboard_ocr)
+
+        menubar = self.menuBar()
+        menubar.addAction(openAction)
+        menubar.addAction(clipboardAction)
+        menubar.addAction(exitAction)
+        # 用来创建窗口内的菜单栏
+        menubar.setNativeMenuBar(False)
+
+        self.widget = QWidget()
+
+        self.lbl_seperate = QLabel('\n'+'-'*100)
+        self.lbl_seperate.setAlignment(Qt.AlignCenter)
+        self.textedit = QTextEdit()
+        self.textedit.setMinimumHeight(300)
+        self.textedit.setMaximumHeight(300)
+        self.lbl_img = QLabel()
+        self.lbl_img.setMaximumHeight(300)
+        self.lbl_img.setAlignment(Qt.AlignCenter)
+        # self.lbl_img.setMinimumHeight(300)
+
+        self.vbox = QVBoxLayout()
+        self.vbox.addWidget(self.textedit)
+        self.vbox.addWidget(self.lbl_seperate)
+        self.vbox.addWidget(self.lbl_img)
+
+        self.widget.setLayout(self.vbox)
+
+        self.setCentralWidget(self.widget)
+        self.setWindowTitle('小龙的OCR识别软件')
+        self.widget.setGeometry(0,0,600,600)
+        self.setWindowIcon(QIcon('ocr.jpg'))
+        self.center()
+        self.show()
+
+
+    def center(self):
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move((screen.width() - size.width()) / 2,
+                  (screen.height() - size.height()) / 2)
+
+    def native_pic_ocr(self):
+        file_path, file_type = QFileDialog.getOpenFileName(self,
+                                                    '选择图片',
+                                                    '',
+                                                    'All Files (*);;Image files(*.jpg *.png *.jpeg *.bmp)')
+        print(file_path)
+        self.do_ocr_and_refresh(file_path)
+
+    def clipboard_ocr(self):
+        file_path = ocr.get_clipboard_image()
+        if not file_path:
+            self.statusBar().showMessage('剪切板中没有图片！')
+        else:
+            self.do_ocr_and_refresh(file_path)
+
+    def do_ocr_and_refresh(self, file_path):
+        self.lbl_img.setPixmap(self.scaled_pixmap(file_path))
+        result = ocr.do_ocr(file_path)
+        self.statusBar().showMessage('结果已复制到剪切板！')
+        self.textedit.setText(result)
+
+    def scaled_pixmap(self, file_path):
+        img_pix = QPixmap(file_path)
+        if img_pix.height() > 300:
+            return img_pix.scaledToHeight(300)
+        else:
+            return img_pix
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = OcrGui()
+    app.exec_()
